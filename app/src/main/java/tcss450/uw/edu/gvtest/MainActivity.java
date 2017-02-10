365971afd9171725053cb898cdb89c3fed0c74db
KaranToor <toork@uw.edu>
2017-02-10 12:52:59 -0800

    Added Verena's code

--- app/src/main/java/tcss450/uw/edu/gvtest/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/gvtest/MainActivity.java
@@ -4,9 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,6 +60,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -86,6 +92,8 @@ public class MainActivity extends AppCompatActivity {
     public static final String FILE_NAME = "temp.jpg";
     private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
     private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
+    private static final String PARTIAL_URL = "http://cssgate.insttech.washington.edu/~ekoval/";
+
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int GALLERY_PERMISSIONS_REQUEST = 0;
@@ -95,11 +103,18 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mImageDetails;
     private ImageView mMainImage;
+    EditText u;
+    EditText p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registerUser(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 
     /**
@@ -108,8 +123,66 @@ public class MainActivity extends AppCompatActivity {
      * @param view The View passed in when this method is called.
      */
     public void logInUser(View view) {
-        Intent intent = new Intent(this, OverviewActivity.class);
-        startActivity(intent);
+        u = (EditText) findViewById(R.id.editText2);
+        p = (EditText) findViewById(R.id.editText3);
+
+        if(u.getText().toString().length() >= 1  && p.getText().toString().length() >= 1 ) {
+            AsyncTask<String, Void, String> task = null;
+            String message = ((EditText) findViewById(R.id.editText2)).getText().toString();
+            String message2 = ((EditText) findViewById(R.id.editText3)).getText().toString();
+
+            task = new GetWebServiceTask();
+            task.execute(PARTIAL_URL, message, message2);
+
+        } else {
+            Toast.makeText(this, "All fields must be filled" ,Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
+    private class GetWebServiceTask extends AsyncTask<String, Void, String> {
+        private final String SERVICE = "login.php";
+        @Override
+        protected String doInBackground(String... strings) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            String url = strings[0];
+            String args = "?username=" + strings[1] + "&password=" + strings[2];
+            try {
+                URL urlObject = new URL(url + SERVICE + args);
+                urlConnection = (HttpURLConnection) urlObject.openConnection();
+                InputStream content = urlConnection.getInputStream();
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = "";
+                while ((s = buffer.readLine()) != null) {
+                    response += s;
+                }
+            } catch (Exception e) {
+                response = "Unable to connect, Reason: "
+                        + e.getMessage();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return response;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            if (result.startsWith("Unable to")) {
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }else if (result.startsWith("{\"Error")){
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG)
+                        .show();
+                return;
+            } else {
+                getApplicationContext().startActivity(new Intent(getApplicationContext(), OverviewActivity.class));
+            }
+
+        }
     }
 
 }
--- app/src/main/java/tcss450/uw/edu/gvtest/SignUpActivity.java
+++ app/src/main/java/tcss450/uw/edu/gvtest/SignUpActivity.java
@@ -1,13 +1,85 @@
 package tcss450.uw.edu.gvtest;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class SignUpActivity extends AppCompatActivity {
+    private static final String PARTIAL_URL = "http://cssgate.insttech.washington.edu/~ekoval/";
+    EditText u;
+    EditText p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
     }
+
+    public void createUser(View view) {
+        u = (EditText) findViewById(R.id.editText4);
+        p = (EditText) findViewById(R.id.editText6);
+        if(u.getText().toString().length() >= 1  && p.getText().toString().length() >= 1 ) {
+            AsyncTask<String, Void, String> task = null;
+            String message = ((EditText) findViewById(R.id.editText)).getText().toString();
+            String message2 = ((EditText) findViewById(R.id.editText4)).getText().toString();
+            String message3 = ((EditText) findViewById(R.id.editText6)).getText().toString();
+            task = new CreatingUserWebServiceTask();
+            task.execute(PARTIAL_URL, message2, message3, message);
+
+        } else {
+            Toast.makeText(this, "All fields must be filled" ,Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class CreatingUserWebServiceTask extends AsyncTask<String, Void, String> {
+        private final String SERVICE = "register.php";
+        @Override
+        protected String doInBackground(String... strings) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            String url = strings[0];
+            String args = "?username=" + strings[1] + "&password=" + strings[2] + "&name=" +strings[3];
+            try {
+                URL urlObject = new URL(url + SERVICE + args);
+                urlConnection = (HttpURLConnection) urlObject.openConnection();
+                InputStream content = urlConnection.getInputStream();
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = "";
+                while ((s = buffer.readLine()) != null) {
+                    response += s;
+                }
+            } catch (Exception e) {
+                response = "Unable to connect, Reason: "
+                        + e.getMessage();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return response;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            if (result.startsWith("Unable to")) {
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG)
+                        .show();
+                return;
+            } else {
+                getApplicationContext().startActivity(new Intent(getApplicationContext(), OverviewActivity.class));
+            }
+
+        }
+    }
+
 }
