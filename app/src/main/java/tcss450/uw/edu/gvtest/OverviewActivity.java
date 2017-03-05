edf59c755979a6d8ee1661404c88d18ea1abaf43
KaranToor <toork@uw.edu>
2017-03-04 21:12:18 -0800

    saving work

--- app/src/main/java/tcss450/uw/edu/gvtest/OverviewActivity.java
+++ app/src/main/java/tcss450/uw/edu/gvtest/OverviewActivity.java
@@ -378,9 +378,16 @@ public class OverviewActivity extends AppCompatActivity implements View.OnLongCl
     }
 
     public String parseLocation(String theInput) {
-        Pattern pattern = Pattern.compile("([^A-Z])(AL|AK|AR|AZ|CA|CO|CT|DC|DE|FL|G" +
+        Pattern pattern = Pattern.compile("(([^A-Z])(AL|AK|AR|AZ|CA|CO|CT|DC|DE|FL|G" +
                 "A|HI|IA|ID|IL|IN|KS|KY|LA|MA|MD|ME|MI|MN|MO|MS|MT|NC|ND|NE|NH|NJ|NM|NV|NY|" +
-                "OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VA|VT|WA|WI|WV|WY)([^A-Z])");
+                "OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VA|VT|WA|WI|WV|WY)([^A-Z]))|(Alabama|Alaska|Arizona|" +
+                "Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|" +
+                "Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|" +
+                "Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New\\sHampshire|" +
+                "New\\sJersey|New\\sMexico|New\\sYork|North\\sCarolina|North\\sDakota|Ohio|" +
+                "Oklahoma|Oregon|Pennsylvania|Rhode\\sIsland|South\\sCarolina|South\\sDakota|" +
+                "Tennessee|Texas|Utah|Vermont|Virginia|Washington|West\\sVirginia|Wisconsin|" +
+                "Wyoming)");
         Matcher matcher = pattern.matcher(theInput);
 
         StringBuilder sb = new StringBuilder();
--- app/src/main/java/tcss450/uw/edu/gvtest/newEntryActivity.java
+++ app/src/main/java/tcss450/uw/edu/gvtest/newEntryActivity.java
@@ -1,9 +1,16 @@
 package tcss450.uw.edu.gvtest;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
 
 
 public class newEntryActivity extends AppCompatActivity {
@@ -26,5 +33,31 @@ public class newEntryActivity extends AppCompatActivity {
         editText.setText(message);
         EditText paymentEdit = (EditText) findViewById(R.id.paymentId);
         paymentEdit.setText(payment);
+        findLastPicture();
+    }
+
+    private void findLastPicture() {
+        // Find the last picture
+        String[] projection = new String[]{
+                MediaStore.Images.ImageColumns._ID,
+                MediaStore.Images.ImageColumns.DATA,
+                MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
+                MediaStore.Images.ImageColumns.DATE_TAKEN,
+                MediaStore.Images.ImageColumns.MIME_TYPE
+        };
+        final Cursor cursor = getApplicationContext().getContentResolver()
+                .query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null,
+                        null, MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
+
+// Put it in the image view
+        if (cursor.moveToFirst()) {
+            final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            String imageLocation = cursor.getString(1);
+            File imageFile = new File(imageLocation);
+            if (imageFile.exists()) {   // TODO: is there a better way to do this?
+                Bitmap bm = BitmapFactory.decodeFile(imageLocation);
+                imageView.setImageBitmap(bm);
+            }
+        }
     }
 }
