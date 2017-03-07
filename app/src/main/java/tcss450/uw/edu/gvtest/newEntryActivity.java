5ac4bbb8b66e208ae9ec0debfc24a2687faec355
Verena <verenan@uw.edu>
2017-03-06 22:25:23 -0800

    retakephoto and email validation

--- app/src/main/java/tcss450/uw/edu/gvtest/newEntryActivity.java
+++ app/src/main/java/tcss450/uw/edu/gvtest/newEntryActivity.java
@@ -1,12 +1,17 @@
 package tcss450.uw.edu.gvtest;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.util.Log;
@@ -21,9 +26,16 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class newEntryActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public static final int GALLERY_PERMISSIONS_REQUEST = 0;
+    public static final int GALLERY_IMAGE_REQUEST = 1;
+    public static final int CAMERA_PERMISSIONS_REQUEST = 2;
+    public static final int CAMERA_IMAGE_REQUEST = 3;
+
     /**
      * The file location of the photo to be used.
      */
@@ -102,6 +114,59 @@ public class newEntryActivity extends AppCompatActivity implements AdapterView.O
             }
         }
 
+        /////////put in if statement? to bottom slashes ////////////////////////////////////////
+        Button retakePhotoButton = (Button) findViewById(R.id.button7);
+        retakePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(newEntryActivity.this);
+                builder
+                        .setMessage(R.string.dialog_select_prompt)
+                        .setPositiveButton(R.string.dialog_select_gallery, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                if (PermissionUtils.requestPermission(newEntryActivity.this, GALLERY_PERMISSIONS_REQUEST,
+                                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                                    Intent intent = new Intent();
+                                    intent.setType(getString(R.string.intenttype));
+                                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                                    startActivityForResult(Intent.createChooser(intent, getString(R.string.choosePhotoPrompt)),
+                                            GALLERY_IMAGE_REQUEST);
+                                }
+                            }
+                        })
+                        .setNegativeButton(R.string.dialog_select_camera, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                try {
+                                    if (PermissionUtils.requestPermission(
+                                            newEntryActivity.this,
+                                            CAMERA_PERMISSIONS_REQUEST,
+                                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                                            Manifest.permission.CAMERA)) {
+                                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(getCameraFile()));
+
+                                        if (Build.VERSION.SDK_INT >= 24) {
+                                            try {
+                                                Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
+                                                m.invoke(null);
+                                            } catch (Exception e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                        startActivityForResult(intent, CAMERA_IMAGE_REQUEST);
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+////////////////////////////////////////////////////////////////////////////////
         if (myOldEntry = intent.getBooleanExtra("fromTable", false)) {
             Button b = (Button) findViewById(R.id.ok_button);
             b.setText(R.string.UpdateStr);
@@ -122,6 +187,36 @@ public class newEntryActivity extends AppCompatActivity implements AdapterView.O
                 spinner.setSelection(spinnerPosition);
             }
         }
+    }
+
+
+
+    private File getCameraFile() throws IOException {
+        // Create an image file name
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences(getString(R.string.prefKey), Context.MODE_PRIVATE);
+        int userid = prefs.getInt(getString(R.string.UID), -1);
+        if (userid == -1) {
+            throw new IllegalArgumentException("userid is not set in Prefs");
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = userid + "_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = File.createTempFile(
+//                imageFileName,  /* prefix */
+//                ".jpg",         /* suffix */
+//                storageDir      /* directory */
+//        );
+
+        File image = new File(
+                storageDir,
+                imageFileName +  /* prefix */
+                        ".jpg"        /* suffix */
+                      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        return image;
     }
 
     public void okButtonPress(View theView) {
